e7c878c9aeabe930c1800312d3c2cd57b1c25889
pinns <emarinepinn@gmail.com>
2023-01-06 09:47:35 +0900

    response fix

--- main.js
+++ main.js
@@ -5,9 +5,12 @@ window.onload = () =>{
 const barblock = async() =>{
     barClassName="_3CTtyx1weTpsYWGGPfcPYc z5cwqEHi5T8D9u8TPHH6m"
     marginClassName="SdEyhW_kE0xoDznTZ5Wr-"
-    await wait(50);
-    Array.prototype.slice.call(document.getElementsByClassName(marginClassName)).forEach((el)=>{el.style.margin='0px';});
-    Array.prototype.slice.call(document.getElementsByClassName(barClassName)).forEach((el)=>{el.remove();});
+    
+    for(let i=0;i<100;i++){
+        Array.prototype.slice.call(document.getElementsByClassName(marginClassName)).forEach((el)=>{el.style.margin='0px';});
+        Array.prototype.slice.call(document.getElementsByClassName(barClassName)).forEach((el)=>{el.remove();});
+        await wait(1);
+    }
 }
 
 const wait = async (ms) => new Promise(resolve => setTimeout(resolve, ms));
